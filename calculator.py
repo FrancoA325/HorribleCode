680f07f1f3541fe06d4da1d80e6c2f85346eda22
Joseph Sinski <joehsinski@icloud.com>
2025-02-17 15:42:53 -0500

    Finished Calc

--- calculator.py
+++ calculator.py
@@ -1,50 +1,48 @@
-def add():
+def add(a,b):
     """Addition of two numbers"""
+    return a+b
 
-
-def subtract():
+def subtract(a,b):
     """Subtraction of two numbers"""
+    return a-b
 
+def divide(a,b):
+    """Divide two numbers"""
+    return a/b
 
-def devide():
-    """Devide two numbers"""
-
-
-def multiply():
+def multiply(a,b):
     """Multiply two numbers"""
+    return a*b
 
-
-def operationsSelection():
+def operationselection():
     """Select the operation to perform and provide inputs
     The function will call the respective function based on the operation selected and pass the inputs to the function.
     """
     print("Select operation and provide two numbers separated by space.")
     print("1. Addition")
     print("2. Subtraction")
-    print("3. Devide")
+    print("3. Divide")
     print("4. Multiply")
 
-    operation, a, b = input(
-        "Enter the operation and two numbers separated by space: "
-    ).split()
+    operation, a, b = input("Enter the operation and two numbers separated by space: ").split()
     a = float(a)
     b = float(b)
 
     if operation == "1":
-        add()
+        print(add(a,b))
     elif operation == "2":
-        subtract()
+        print(subtract(a,b))
     elif operation == "3":
-        devide()
+        print(divide(a,b))
     elif operation == "4":
-        multiply()
+        print(multiply(a,b))
     else:
         print("Invalid operation selected.")
 
 
 def main():
     """Main function to run the program"""
-    operationsSelection()
+    operationselection()
 
 
 if __name__ == "__main__":
